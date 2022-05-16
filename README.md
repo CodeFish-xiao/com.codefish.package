1e76bfd1cad68701d796874a8336b59ea25d24c9
codefish <1109038583@qq.com>
2022-05-17 01:00:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## 自定义包测试版
+- 本包是用来简单定义并且测试Unity Package Manager
---
+++ Runtime/Fuck/Fuck.cs
@@ -0,0 +1,15 @@
+﻿namespace Fuck;
+
+interface IFuck
+{
+    // 接口成员
+    void Fuck();
+}
+
+class FuckImpl : IFuck
+{ 
+    public void Fuck()
+    {
+        Console.WriteLine("fuck");
+    }
+}
